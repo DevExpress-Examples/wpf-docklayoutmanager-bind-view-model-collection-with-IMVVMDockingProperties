82c5c85ff1ce388ddd509141bb59af8434507612
DevExpressExampleBot <fakeEmail>
2021-12-08 13:03:57 +0300

    Source auto update [skip ci]

--- VB/DXSample/ViewModels/MainViewModel.vb
+++ VB/DXSample/ViewModels/MainViewModel.vb
@@ -1,73 +1,59 @@
-﻿Imports System.Collections.ObjectModel
+Imports System.Collections.ObjectModel
 Imports DevExpress.Mvvm
 Imports DevExpress.Xpf.Docking
 
 Namespace DXSample.ViewModels
-	Public Class MainViewModel
-		Inherits ViewModelBase
 
-		Public Property Panels() As ObservableCollection(Of PanelViewModel)
-			Get
-				Return GetValue(Of ObservableCollection(Of PanelViewModel))()
-			End Get
-			Set(ByVal value As ObservableCollection(Of PanelViewModel))
-				SetValue(value)
-			End Set
-		End Property
+    Public Class MainViewModel
+        Inherits ViewModelBase
 
-		Public Sub New()
-			Panels = New ObservableCollection(Of PanelViewModel)() From {
-				New PanelViewModel() With {
-					.Caption = "One",
-					.Content = "A regular panel",
-					.TargetName = "documentGroup"
-				},
-				New PanelViewModel() With {
-					.Caption = "Two",
-					.Content = "A regular panel",
-					.TargetName = "documentGroup"
-				},
-				New PanelViewModel() With {
-					.Caption = "Three",
-					.Content = "A regular panel",
-					.TargetName = "documentGroup"
-				},
-				New PanelViewModel() With {
-					.Caption = "Five",
-					.Content = "A panel",
-					.TargetName = "layoutGroup"
-				}
-			}
-		End Sub
-	End Class
+        Public Property Panels As ObservableCollection(Of PanelViewModel)
+            Get
+                Return GetValue(Of ObservableCollection(Of PanelViewModel))()
+            End Get
 
-	Public Class PanelViewModel
-		Inherits ViewModelBase
-		Implements IMVVMDockingProperties
+            Set(ByVal value As ObservableCollection(Of PanelViewModel))
+                SetValue(value)
+            End Set
+        End Property
 
-		Public Property Caption() As String
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Content() As String
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property TargetName() As String Implements IMVVMDockingProperties.TargetName
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-			End Set
-		End Property
-	End Class
+        Public Sub New()
+            Panels = New ObservableCollection(Of PanelViewModel)() From {New PanelViewModel() With {.Caption = "One", .Content = "A regular panel", .TargetName = "documentGroup"}, New PanelViewModel() With {.Caption = "Two", .Content = "A regular panel", .TargetName = "documentGroup"}, New PanelViewModel() With {.Caption = "Three", .Content = "A regular panel", .TargetName = "documentGroup"}, New PanelViewModel() With {.Caption = "Five", .Content = "A panel", .TargetName = "layoutGroup"}}
+        End Sub
+    End Class
+
+    Public Class PanelViewModel
+        Inherits ViewModelBase
+        Implements IMVVMDockingProperties
+
+        Public Property Caption As String
+            Get
+                Return GetValue(Of String)()
+            End Get
+
+            Set(ByVal value As String)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Content As String
+            Get
+                Return GetValue(Of String)()
+            End Get
+
+            Set(ByVal value As String)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property TargetName As String Implements IMVVMDockingProperties.TargetName
+            Get
+                Return GetValue(Of String)()
+            End Get
+
+            Set(ByVal value As String)
+                SetValue(value)
+            End Set
+        End Property
+    End Class
 End Namespace
